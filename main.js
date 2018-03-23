bd1bb6d8dbc19a55f313c93bca92bb1b5d1e0f66
Camus LEJARRE <camus.lejarre@mairie-saint-maur.com>
2018-03-23 09:58:15 +0100

    - ajout de plusieurs applications
    - lecture des fichiers .status pour actualiser le statut de l'appli

--- main.js
+++ main.js
@@ -1,54 +1,111 @@
 $(function () {
-
+    "use strict";
 	load_list();
 	$('#search-field').on('input propertychange paste', load_list);
 });
-	window.FontAwesomeConfig = {
-		searchPseudoElements: true
-	}
-function compare(a,b) {
-  return a.name.localeCompare(b.name)
+
+
+/*jslint indent: 4 */
+/*global $, jQuery, window */
+window.FontAwesomeConfig = {
+	searchPseudoElements: true
+};
+
+function compareName(a, b) {
+    "use strict";
+    return a.name.localeCompare(b.name);
 }
 
-function load_list(){
-	jQuery.getJSON('applis.conf', function(APPLIS) {
-		HTML = '';
-		//Récupérer les paramètres de l'appli Default
-		DEFAULT = APPLIS.applis[0];
-		applis = APPLIS.applis.sort(compare);
-		applis.forEach(function (obj, index){
-			if((obj.name.search(new RegExp($('#search-field').val(), "i")) >= 0 || obj.tags.search(new RegExp($('#search-field').val(), "i")) >= 0 || $('#search-field').val() == '') && obj.name != 'Default'){
-				HTML += "<li class='applis_item ";
-				if (obj.icon != ''){
-					ICON = obj.icon;
-				}else{
-					ICON = DEFAULT.icon;
-				}
-				//Ajout de classes pour le statut
-				// STATUT = true; //Utiliser les URI
-				STATUT = Math.round(Math.random()*2); //Génère des statuts aléatoires pour test
-				switch(STATUT) {
-					case 0:
-						HTML += 'app_down';
-						// ICON = "./img/down.png";
-						DESC = "Appli en panne !"
-						break;
-					case 2:
-						HTML += 'app_maintenance';
-						// ICON = "./img/warning.png";
-						DESC = "Appli en maintenance, de retour bientôt..."
-						break;
-					case 1:
-					default:
-						HTML += 'app_up';
-						DESC = obj.desc;
-				}
-				HTML += "' title='"+DESC+"'><a href='"+obj.url+"' target='_BLANK'>";
-				HTML+= "<img src='"+ICON+"'>";
-				HTML += obj.name;
-				HTML += "</a></li>";
-			}
+function compareCat(a, b) {
+    "use strict";
+	return a.cat - b.cat;
+}
+
+function load_list() {
+    "use strict";
+	
+	//Noms des catégories
+	var catNames = ["Pour tous...", "Administratif", "Etat Civil, Elections", "Ressources & Médias", "DSI", "Culture", "Espace public"];
+	
+	jQuery.getJSON('applis.conf', function (APPLIS) {
+		
+		var HTML = '', catSorted = [], DEFAULT = APPLIS.applis[0], applis = APPLIS.applis.sort(compareCat);
+		//Ranger les applis par catégorie
+		applis.forEach(function (cat) {
+			if (catSorted[cat.cat] === void 0) { catSorted[cat.cat] = []; }
+			catSorted[cat.cat].push(cat);
 		});
-		$('.list_applis').html(HTML);
+		//Parcourir les catégories
+		catSorted.forEach(function (thisCat, index) {
+			//Ecrire le titre et créer la liste
+			var UL = "<ul class='list_applis'>";
+			//Parcourir les applis
+			thisCat = thisCat.sort(compareName);
+			//vérification du nombre d'applis
+			var NB = 0;
+			thisCat.forEach(function (obj) {
+				if ((obj.name.search(new RegExp($('#search-field').val(), "i")) >= 0 || obj.tags.search(new RegExp($('#search-field').val(), "i")) >= 0 || $('#search-field').val() === '') && obj.name !== 'Default') {
+					NB++;
+					UL += "<li class='applis_item ";
+					var ICLASS = '', DESC = '', ICON = '';
+					if (obj.icon !== '') {
+						ICON = obj.icon;
+					} else {
+						ICON = DEFAULT.icon;
+					}
+					//Ajout de classes pour le statut		
+					var STATUT = null;
+					if(obj.status_file !== void 0){
+						$.ajax({
+							url :'app_status/'+obj.status_file,
+							async: false,
+							dataType: "text",
+							success: function(data) {
+								STATUT = data.split('\n')[0];
+							}
+						});
+					}
+					
+					console.log('after: '+obj.name+" "+STATUT+"camus");
+					switch (STATUT) {
+					case 'CRITICAL':
+						UL += 'app_down';
+						ICLASS = "fa-exclamation-circle";
+						DESC = "Cette application est indisponible.";
+						break;
+					case 'OK':
+						console.log("OK: "+STATUT);
+						UL += 'app_up';
+						ICLASS = "";
+						DESC = obj.desc;
+						break;
+					case 'WARNING':
+						UL += 'app_maintenance';
+						ICLASS = "fa-cog fa-spin";
+						DESC = "Maintenance en cours,<br>de retour bientôt...";
+						break;
+					/*case 3:
+						UL += 'app_unreachable';
+						ICLASS = "fa fa-question-circle";
+						// ICON = "./img/warning.png";
+						DESC = "Appli injoignable";
+						break;*/
+					case 'UNKNOWN':
+					default:
+						UL += 'app_no_status';
+						ICLASS = "";
+						DESC = obj.desc;
+					}
+					UL += "'><a class='main_link' href='" + obj.url + "' target='_BLANK' title=\"" + DESC + "\">";
+					UL += "<img src='" + ICON + "'>";
+					UL += "<span class='app_name'>" + obj.name + "</span></a>";
+					UL += "<a class='fill_cont' href='" + obj.url + "' target='_BLANK' title=\"" + DESC + "\"><div class='right_img'></div><div class='filler'><i class='fa fa-lg " + ICLASS + "'></i><span class='text'>" + DESC + "</span></div></a></li>";
+				}
+			});
+			UL += "</ul>";
+			//S'il n'y a aucune application, on n'affiche pas la catégorie.
+			if(NB > 0) HTML += "<h2>" + catNames[index] + " ("+NB+")</h2>"+UL;
+		});
+		$('.applis_cont').html(HTML);
 	});
 }
